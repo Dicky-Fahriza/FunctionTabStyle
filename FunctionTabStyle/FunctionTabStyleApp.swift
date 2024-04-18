62426854b9dbf9719e2245ea24965f08576c8e4c
Dicky-Fahriza <dickyfahriza46@gmail.com>
2024-04-18 10:01:37 +0700

    Initial Commit

---
+++ FunctionTabStyle/FunctionTabStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunctionTabStyleApp.swift
+//  FunctionTabStyle
+//
+//  Created by MacBook Pro on 18/04/24.
+//
+
+import SwiftUI
+
+@main
+struct FunctionTabStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
